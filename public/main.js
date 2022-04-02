20765f16d23b7203a1b4260aa70501adb5793b97
yusuke_nakajima <www.yyworld@gmail.com>
2022-04-03 01:50:50 +0900

    タブ パターン1を実装

---
+++ public/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/ts/main.ts ***!
+  \************************/
+console.log('hello hello');
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
